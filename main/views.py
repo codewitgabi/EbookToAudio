21da6e1cfe72f7f290ab3810916ff4fde14a0397
Gabriel Michael <gabrielmicheal497@gmail.com>
2022-12-30 19:47:14 +0100

    Updated program to only download audio files rather than read them online

--- main/views.py
+++ main/views.py
@@ -6,19 +6,68 @@ import json
 import os
 
 
-def read_text_file(file):
+def index(request):
+    return render(request, "main/index.html")
+
+
+# def read_text_file(file):
+#     try:
+#         with open(file, "r") as f:
+#             content = f.read()
+#             eng = pyttsx3.init()
+#             eng.say(content)
+#             eng.runAndWait()
+
+#     except FileNotFoundError:
+#         pass
+
+
+# def read_PDF_file(file):
+#     try:
+#         with open(file, "rb") as pdfFile:
+#             pdfObj = PyPDF2.PdfReader(pdfFile)
+#             eng = pyttsx3.init()
+
+#             i = 0
+#             while i < len(pdfObj.pages):
+#                 page = pdfObj.pages[i]
+
+#                 eng.say(page.extract_text())
+#                 eng.runAndWait()
+#                 i += 1
+
+#     except UnicodeDecodeError:
+#         pass
+#     except FileNotFoundError:
+#         pass
+
+
+# def read_file(request):
+#     data = json.loads(request.body)
+#     file = data.get("file")
+#     extension = file.split(".")[-1]
+
+#     if extension == "txt":
+#         read_text_file(file)
+#     elif extension == "pdf":
+#         read_PDF_file(file)
+
+#     return JsonResponse("Done", safe= False)
+
+
+def save_txt_audio(file):
     try:
         with open(file, "r") as f:
             content = f.read()
             eng = pyttsx3.init()
-            eng.say(content)
+            download_path = os.path.relpath(file).split(f"{os.path.sep}")[-1].split(".")[0]
+            eng.save_to_file(content, f"{os.path.expanduser('~')}{os.path.sep}Downloads{os.path.sep}{download_path}.mp3")
             eng.runAndWait()
 
     except FileNotFoundError:
         pass
 
-
-def read_PDF_file(file):
+def save_pdf_audio(file):
     try:
         with open(file, "rb") as pdfFile:
             pdfObj = PyPDF2.PdfReader(pdfFile)
@@ -27,8 +76,7 @@ def read_PDF_file(file):
             i = 0
             while i < len(pdfObj.pages):
                 page = pdfObj.pages[i]
-
-                eng.say(page.extract_text())
+                eng.save_to_file(page.extract_text(), f"{os.path.expanduser('~')}{os.path.sep}Downloads{os.path.sep}page{i + 1}.mp3")
                 eng.runAndWait()
                 i += 1
 
@@ -36,37 +84,6 @@ def read_PDF_file(file):
         pass
     except FileNotFoundError:
         pass
-
-def index(request):
-    return render(request, "main/index.html")
-
-
-def read_file(request):
-    data = json.loads(request.body)
-    file = data.get("file")
-    extension = file.split(".")[-1]
-
-    if extension == "txt":
-        read_text_file(file)
-    elif extension == "pdf":
-        read_PDF_file(file)
-
-    return JsonResponse("Done", safe= False)
-
-
-def save_txt_audio(file):
-    try:
-        with open(file, "r") as f:
-            content = f.read()
-            eng = pyttsx3.init()
-            eng.save_to_file(content, f"{os.path.expanduser('~')}{os.path.sep}Downloads{os.path.sep}project.mp3")
-            eng.runAndWait()
-
-    except FileNotFoundError:
-        pass
-
-def save_pdf_audio(file):
-    pass
 
 def save_audio(request):
     data = json.loads(request.body)
