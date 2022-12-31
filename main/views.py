5d6243cd4df3e4aa1bc82145685983f6dadb9876
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2022-12-31 11:49:54 +0000

    Updated download path

--- main/views.py
+++ main/views.py
@@ -15,9 +15,11 @@ def save_txt_audio(file):
         with open(file, "r") as f:
             content = f.read()
             eng = pyttsx3.init()
-            download_path = os.path.relpath(file).split(f"{os.path.sep}")[-1].split(".")[0]
-            eng.save_to_file(content, f"{os.path.expanduser('~')}{os.path.sep}Downloads{os.path.sep}{download_path}.mp3")
-            print(f"{os.path.expanduser('~')}{os.path.sep}Downloads{os.path.sep}{download_path}.mp3")
+            #download_path = os.path.relpath(file).split(f"{os.path.sep}")[-1].split(".")[0]
+            download_path = os.path.dirname(file)
+            filename = file.split(".txt")[0].split(os.path.sep)[-1])
+            eng.save(content, download_path + os.path.sep + filename + ".mp3")
+            #eng.save_to_file(content, f"{os.path.expanduser('~')}{os.path.sep}Downloads{os.path.sep}{download_path}.mp3")
             eng.runAndWait()
 
     except FileNotFoundError:
